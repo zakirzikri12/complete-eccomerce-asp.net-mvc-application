a2c4b8a06ae323fcbe3fc27642ae71a9fb1ddfbf
zakirkx12 <80703252+zakirzikri12@users.noreply.github.com>
2024-03-18 16:31:47 +0800

    added New Movie View Model

---
+++ Models/ViewModels/NewMovieDropdownsVM.cs
@@ -0,0 +1,21 @@
+﻿using eTikects.Models;
+
+namespace eTikects.Models.ViewModels
+{
+    public class NewMovieDropdownsVM
+    {
+        public NewMovieDropdownsVM()
+        {
+            Producers = new List<Producer>();
+            Cinemas = new List<Cinema>();
+            Actors = new List<Actor>();
+
+        }
+
+        public List<Cinema> Cinemas { get; set; }
+        public List<Producer> Producers { get; set; }
+        public List<Actor> Actors { get; set; }
+
+
+    }
+}
