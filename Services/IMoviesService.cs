898d07be8fbeff2f348c359045298fab9bed2586
zakirkx12 <80703252+zakirzikri12@users.noreply.github.com>
2024-03-18 16:32:20 +0800

    added movie service

---
+++ Services/IMoviesService.cs
@@ -0,0 +1,18 @@
+﻿using eTikects.Data.Base;
+using eTikects.Models;
+using eTikects.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eTickets.Data.Services
+{
+    public interface IMoviesService : IEntityBaseRepository<Movie>
+    {
+        Task<Movie> GetMovieByIdAsync(int id);
+        Task<NewMovieDropdownsVM> GetNewMovieDropdownsValues();
+        Task AddNewMovieAsync(NewMovieVM data);
+        Task UpdateMovieAsync(NewMovieVM data);
+    }
+}
